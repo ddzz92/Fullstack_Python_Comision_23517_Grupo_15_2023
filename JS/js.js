87c2dd8ac102e5ab6b566dc1f13e6d3ebf1ae710
ddzz92 <damiandeseta14@gmail.com>
2023-10-25 19:34:50 -0300

    Intento restore

--- JS/js.js
+++ JS/js.js
@@ -1,3 +1,5 @@
+// Función de la sección Cotizador
+
 function cotizador(tamañoLargo, tamañoAncho, colores, tinta, detalle) {
     let valor = 0;
 
@@ -29,3 +31,25 @@ formulario.addEventListener("submit", function (event) {
 });
 
 // Función de cambiar colores de la página para el modo oscuro
+
+
+// Función del mapa de Google
+function iniciarMap(){
+    var coord = {lat:-34.5956145 ,lng: -58.4431949};
+    var map = new google.maps.Map(document.getElementById('map'),{
+      zoom: 10,
+      center: coord
+    });
+    var marker = new google.maps.Marker({
+      position: coord,
+      map: map
+    });
+}
+
+// Función para el botón de la barra de Menú
+document.getElementById("icon-menu").addEventListener("click", mostrar_menu);
+
+function mostrar_menu(){
+    document.getElementById("move-content").classList.toggle('move-container-all');
+    document.getElementById("show-menu").classList.toggle('show-lateral');
+}
